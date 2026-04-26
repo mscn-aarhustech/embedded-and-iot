96f6d1e91fda7b7473f5c825ace10898d8a8cd6f
Michael Schmidt Nissen <mscn@aarhustech.dk>
2026-04-26 12:31:20 +0200

    added kitronik motor driver example

---
+++ microbit/kitronik-compact-motor-driver/kitronik-motor-driver-examples-01-main.py
@@ -0,0 +1,80 @@
+from microbit import *
+
+# Define motor control pins
+MOTOR1_PIN1 = pin12
+MOTOR1_PIN2 = pin8
+MOTOR2_PIN1 = pin16
+MOTOR2_PIN2 = pin0
+
+# Clear pins
+MOTOR1_PIN1.write_digital(0)
+MOTOR1_PIN2.write_digital(0)
+MOTOR2_PIN1.write_digital(0)
+MOTOR2_PIN2.write_digital(0)
+
+# Motor controller
+def motor_run(pin1, pin2, speed):
+    speed = max(-1, min(1, speed))
+    
+    if speed > 0:
+        pin1.write_analog(int(speed * 1023))
+        pin2.write_digital(0)  
+    elif speed < 0:
+        pin1.write_digital(0) 
+        pin2.write_analog(int(-speed * 1023))
+    else:
+        pin1.write_digital(0)
+        pin2.write_digital(0)
+
+# Motor commands
+while True:
+
+    # Rotate left at full speed
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, 1.0)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, -1.0)
+    
+    sleep(2000)
+
+    # Stop
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, 0.0)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, 0.0)
+
+    sleep(500)
+
+    # Rotate right at half speed
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, -0.5)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, 0.5)
+    
+    sleep(2000)
+
+    # Stop
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, 0.0)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, 0.0)
+
+    sleep(500)
+
+    # Forward at full speed
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, 1.0)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, 1.0)
+    
+    sleep(2000)
+
+    # Stop
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, 0.0)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, 0.0)
+
+    sleep(500)
+
+    # Reverse at half speed
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, -0.5)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, -0.5)
+    
+    sleep(2000)
+
+    # Stop
+    motor_run(MOTOR1_PIN1, MOTOR1_PIN2, 0.0)
+    motor_run(MOTOR2_PIN1, MOTOR2_PIN2, 0.0)
+
+    sleep(500)
+
+  
