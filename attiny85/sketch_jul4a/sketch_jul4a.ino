aaf4523047d09f5fadc5de31d6e5e3f86f73162a
Michael Schmidt Nissen P16s <mscn@aarhustech.dk>
2026-08-05 21:31:46 +0200

    attiny snippet

---
+++ attiny85/sketch_jul4a/sketch_jul4a.ino
@@ -0,0 +1,19 @@
+#include <SoftwareSerial.h>
+
+#define TX_PIN 3
+#define RX_PIN 4
+
+SoftwareSerial mySerial(RX_PIN, TX_PIN); // RX, TX
+
+void setup() {
+  mySerial.begin(9600);
+}
+
+int n = 0;
+
+void loop() {
+  mySerial.println("Hello, world!");
+  mySerial.println(String(n));
+  n += 1;
+  delay(1000);
+}
